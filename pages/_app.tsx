c4a4376daf195e1272dda198235f8baa27e2a7e1
Arpan Mondal <arpan.m@pm.me>
2022-07-14 15:14:14 +0000

    Layout Done #4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,13 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div className='bg-gradient-to-r from-indigo-700 via-purple-500 to-pink-600 grid grid-cols-12 gap-4 lg:gap-8 p-8 lg:p-12 h-screen'>
+    <div className='lg:col-span-3 col-span-12 bg-slate-50 dark:bg-slate-900 rounded-2xl p-4 lg:p-6'>      Sidebar</div>
+    <div className='lg:col-span-9 col-span-12 bg-slate-50 dark:bg-slate-900 rounded-2xl p-4 lg:p-6'>
+    <Component {...pageProps} />
+    </div>
+
+  </div>
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,14 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import React from 'react'
 
-const Home: NextPage = () => {
+const About = () => {
   return (
     <div>
-      <Head>
-        <title>Change Me</title>
-        <meta name="description" content="Next.js starter template with typescript & tailwindCSS" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main>
-        <div className="justify-center font-extrabold text-center text-7xl mx-10 px-5">
-        <h1>
-          Front-end Starter Template with Next.js ,Typescript ,tailwindCSS!
-        </h1>
-        </div>
-        
-
-        <p className="justify-center font-extrabold text-center text-2xl ">
-          Get started by editing{' '}
-          <code>pages/index.tsx</code>
-        </p>
-
-        
+        About Me
       </main>
-
+      
     </div>
   )
 }
 
-export default Home
+export default About
