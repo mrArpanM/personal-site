cfa55fc504f2180c863050c17e349c53eebe39d5
Arpan Mondal <arpan.m@pm.me>
2022-07-15 12:47:34 +0000

    Sidebar Updated #7

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import Image from 'next/image'
+import mypic from '../public/dp.jpg'
 
 const Sidebar = () => {
   return (
     <div>
-        <img src="" alt="Arpan M avatar" />
-        <h3>Arpan M</h3>
+      <div className='flex justify-center p-4'>
+      <div className='rounded-full px-1.5 pt-1.5 bg-emerald-500 shadow-2xl shadow-emerald-600 hover:bg-pink-700 hover:shadow-pink-800 bg-blend-lighten'>
+      <Image
+            src={mypic}
+            alt="Avatar of Arpan M"
+            width="150px"
+            height="150px"
+            className='rounded-full'
+            placeholder='blur'
+      />
+      </div>
+      </div>
+        <div className='flex justify-center font'>
+          <h2>Arpan M with ❤ </h2>
+        </div>
         
     </div>
   )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,18 @@
 import '../styles/globals.css'
 import Sidebar from '../components/sidebar'
-import About from '../components'
+import About from '../components/dashboard'
 
 function MyApp(){
-  return <div className='bg-gradient-to-r from-indigo-700 via-purple-500 to-pink-600 grid grid-cols-12 gap-4 lg:gap-8 p-8 lg:p-12 h-screen'>
-    <div className='lg:col-span-3 col-span-12 bg-slate-50 dark:bg-slate-900 rounded-2xl p-4 lg:p-6'>
+  return <div>
+
+  <div className='bg-gradient-to-r from-indigo-700 via-purple-500 to-pink-600 grid grid-cols-12 gap-4 lg:gap-8 p-8 lg:p-12 h-screen'>
+    <div className='lg:col-span-3 col-span-12  bg-slate-50 dark:bg-slate-900 rounded-2xl p-4 lg:p-6'>
       < Sidebar/>
     </div>
     <div className='lg:col-span-9 col-span-12 bg-slate-50 dark:bg-slate-900 rounded-2xl p-4 lg:p-6'>
     < About/>
     </div>
-
+    </div>
   </div>
 }
 
