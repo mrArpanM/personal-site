14a4c6b8e3c6738d431ef23fbce61b3665df62eb
Arpan Mondal <arpan.m@pm.me>
2022-07-15 06:39:24 +0000

    Components updatad #6

---
+++ components/sidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>
+        <img src="" alt="Arpan M avatar" />
+        <h3>Arpan M</h3>
+        
+    </div>
+  )
+}
+
+export default Sidebar  
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Sidebar from '../components/sidebar'
+import About from '../components'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp(){
   return <div className='bg-gradient-to-r from-indigo-700 via-purple-500 to-pink-600 grid grid-cols-12 gap-4 lg:gap-8 p-8 lg:p-12 h-screen'>
-    <div className='lg:col-span-3 col-span-12 bg-slate-50 dark:bg-slate-900 rounded-2xl p-4 lg:p-6'>      Sidebar</div>
+    <div className='lg:col-span-3 col-span-12 bg-slate-50 dark:bg-slate-900 rounded-2xl p-4 lg:p-6'>
+      < Sidebar/>
+    </div>
     <div className='lg:col-span-9 col-span-12 bg-slate-50 dark:bg-slate-900 rounded-2xl p-4 lg:p-6'>
-    <Component {...pageProps} />
+    < About/>
     </div>
 
   </div>
